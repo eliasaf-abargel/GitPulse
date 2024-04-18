a108f64a727ce9a64dd4025054f95313c37d965b
eliasaf-abargel <eliasafabargel@icloud.com>
2024-04-19 01:31:39 +0300

    "change file config.js"

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -2,8 +2,10 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+const mongoURI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster01.9jdwrvy.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
+
 module.exports = {
-  mongoURI: `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster01.9jdwrvy.mongodb.net/GitDash`,
+  mongoURI,
   githubToken: process.env.GITHUB_TOKEN,
   organizationName: process.env.ORGANIZATION_NAME,
   jwtSecret: process.env.JWT_SECRET,
